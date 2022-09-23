bf14b0ef29081e08cf531fbad7fbee026a812dc9
BillyRonico <billyronico412@gmail.com>
2022-09-23 16:17:32 +0200

    Add viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "./store";
-import { getTraductionByFr, LangEnum } from "./utils";
+import { getTraductionByFr } from "./utils";
 
 const App = () => {
     const navigate = useNavigate();
     const lang = useSelector((state: RootState) => state.app.lang);
     return (
-        <main className="h-screen flex flex-col text-5xl font-black tracking-wide">
+        <main className="h-screen flex flex-col  font-black tracking-wide">
             <div
-                className="flex-grow flex justify-center items-center bg-black text-white cursor-pointer"
+                className="flex-grow flex justify-center text-5xl items-center bg-black text-white cursor-pointer"
                 onClick={() => {
                     navigate("/action");
                 }}
@@ -17,7 +17,7 @@ const App = () => {
                 {getTraductionByFr("action", lang)}
             </div>
             <div
-                className="flex-grow flex justify-center items-center cursor-pointer"
+                className="flex-grow flex justify-center text-5xl items-center cursor-pointer"
                 onClick={() => {
                     navigate("/verite");
                 }}
